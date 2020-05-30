de1a33b06dde6db0149bd331cf32637555819c33
Michael <polli104@mail.chapman.edu>
2020-05-30 10:43:31 -0700

    added gitpod configuration

---
+++ README.md
@@ -0,0 +1,6 @@
+# Parkitect Modding Community Website
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io#https://github.com/parkitect-mod-community/Website)	
+
+https://parkitect-mod-community.github.io/Website/
+
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,17 +21,17 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
+  title: 'Parkitect Mod Community', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/Website/', // Base URL for your project */
+  url: 'https://parkitect-mod-community.github.io/', // Your website URL
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
   projectName: 'Website',
-  organizationName: 'PollindParkitectMods',
+  organizationName: 'parkitect-mod-community',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
