6622294b27f1ad9bfb1856aea4734db1185036de
Michael <polli104@mail.chapman.edu>
2020-05-30 11:24:45 -0700

    change to org page

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,14 +23,14 @@ const users = [
 const siteConfig = {
   title: 'Parkitect Mod Community', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://parkitect-mod-community.github.io/docs', // Your website URL
+  url: 'https://parkitect-mod-community.github.io', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'Website',
+  projectName: 'parkitect-mod-community.github.io',
   organizationName: 'parkitect-mod-community',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
